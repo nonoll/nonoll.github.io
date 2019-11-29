55aa980afd0aa32fe9cd0c8d1e5822a4808d94d7
nonoll <hkroh@wemakeprice.com>
2019-11-29 12:22:27 +0900

    test: build test

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,6 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people test</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
